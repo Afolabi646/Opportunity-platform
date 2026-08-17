a5fcaa65aff54d88f9ecfac1d2d7a97029d9039e
Afolabi <devafolabi646@gmail.com>
2026-08-17 10:38:28 +0100

    Add user authentication

--- home/views.py
+++ home/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Opportunity
 
 def index(request):
-    opportunities = Opportunity.objects.all()
+    query = request.GET.get("q", "")
+
+    if query:
+        opportunities = Opportunity.objects.filter(
+            Q(title__icontains=query) |
+            Q(organization__icontains=query) |
+            Q(description__icontains=query) |
+            Q(requirements__icontains=query) |
+            Q(category__icontains=query) |
+            Q(location__icontains=query) 
+        )
+    else:
+        opportunities = Opportunity.objects.all()
 
 
     return render(request, "home/index.html", {
-        "opportunities": opportunities
+        "opportunities": opportunities,
+        "query" : query
     })
 
 def opportunity_detail(request, pk):
